8d0dccbf2ade71456927d78f8f9536a6734bf353
futami-dhikusu <a-futami@ndics.co.jp>
2025-08-27 16:27:17 +0900

    ログファイルの追加

--- pytest_test/test_main.py
+++ pytest_test/test_main.py
@@ -3,10 +3,12 @@
 
 from main import add, multiply
 
+
 def test_add():
     """add関数のテスト"""
     assert add(2, 3) == 5  # 正しい結果の場合、テストは成功
     assert add(-1, 1) == 0
+
 
 def test_multiply():
     """multiply関数のテスト"""
